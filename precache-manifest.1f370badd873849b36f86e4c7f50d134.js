9f6dc266d452c91530cb29d70a72c51749713d10
Anna Arkhipenko <ak.dev@protonmail.com>
2019-08-27 20:18:37 +0300

    INITIAL COMMIT

---
+++ precache-manifest.1f370badd873849b36f86e4c7f50d134.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1da8ba28c3f01532898da37efcae651d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcc1d4db69c68e233a58",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "18d43606687b6ad00029",
+    "url": "/static/js/2.5c43e2a2.chunk.js"
+  },
+  {
+    "revision": "dcc1d4db69c68e233a58",
+    "url": "/static/js/main.237f3d54.chunk.js"
+  },
+  {
+    "revision": "bf5983c67b612b4ae63b",
+    "url": "/static/js/runtime~main.0dce31f5.js"
+  }
+]);
